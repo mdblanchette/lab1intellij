3b005dac2c508de06013baf3fdda1ff7f7986a1c
Michael Blanchette <mdblanchette@connect.ust.hk>
2022-11-06 13:53:57 +0800

    made a bitchass change

--- src/main/java/Lab1/Main.java
+++ src/main/java/Lab1/Main.java
@@ -2,7 +2,9 @@ package Lab1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main min = new Main();
+        if (min.someLibraryMethod())
+                System.out.println("Hello, world!");
     }
 
     public boolean someLibraryMethod() {
