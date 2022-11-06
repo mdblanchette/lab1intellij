6e4aa26bc3cf6158604bbaec220188f2e404f225
Michael Blanchette <mdblanchette@connect.ust.hk>
2022-11-06 13:48:09 +0800

    setup

---
+++ src/main/java/Lab1/Main.java
@@ -0,0 +1,11 @@
+package Lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
